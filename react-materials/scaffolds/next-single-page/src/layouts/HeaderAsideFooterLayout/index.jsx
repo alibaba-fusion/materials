02b5a6b1d3e7d8901776bbf85ad39964489054db
暮尘 <i@muchen.me>
2018-11-22 20:14:26 +0800

    feat: mod file struct

--- react-materials/scaffolds/next-single-page/src/layouts/HeaderAsideFooterLayout/index.jsx
+++ react-materials/scaffolds/next-single-page/src/layouts/HeaderAsideFooterLayout/index.jsx
@@ -1,30 +1,21 @@
 'use strict';
 
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import Header from 'components/header/index';
 import SideMenu from 'components/side-menu/index';
-import classNames from 'classnames';
-import {asideMenuConfig} from '../../pages/menuConfig';
+// import classNames from 'classnames';
+import { asideMenuConfig } from '../../menuConfig';
 
 import './index.scss';
 
 class Layout extends React.Component {
-  static propTypes = {
-    children: PropTypes.node,
-  }
-
-  constructor(props) {
-    super(props);
-  }
-
-
   render() {
     return (
       <div className="header-aside-footer-layout">
         <Header />
         <div className="body">
-          <SideMenu dataSource={asideMenuConfig} className="aside"/>
+          <SideMenu dataSource={asideMenuConfig} className="aside" />
           <div className="main">
             {this.props.children}
           </div>
