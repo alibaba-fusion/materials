7d3eaa8c5ad73225bfc2d61122e8090e5a817185
nianxiongdi <41629087+nianxiongdi@users.noreply.github.com>
2019-02-28 16:07:01 +0800

    Update index.jsx

--- scaffolds/next-single-page-a11y/src/pages/page2/index.jsx
+++ scaffolds/next-single-page-a11y/src/pages/page2/index.jsx
@@ -36,7 +36,6 @@ class Page2 extends React.Component {
                   title={item[0]}
                   content={item[1]}
                   onClick={this.stepItemClick}
-                  role="button"
                 />
               ))}
             </Step>
