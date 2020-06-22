4f677c50134935dcb568ec051dd577c59faf427a
笑斌 <xinlei.sxl@alibaba-inc.com>
2020-06-22 11:34:14 +0800

    fix: duplicate primary key

--- blocks/SingleTreeTable/src/index.tsx
+++ blocks/SingleTreeTable/src/index.tsx
@@ -4,125 +4,150 @@ import styles from './index.module.scss';
 
 
 const data = [{
+  id: 'first',
   rowNo: '中华人民共和国国内安全管理条例',
   rowContent: '',
   children: [
     {
+      id: 1,
       rowNo: '第一条',
       // eslint-disable-next-line
       rowContent: '法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容',
     },
     {
+      id: 2,
       rowNo: '第二条',
       // eslint-disable-next-line
       rowContent: '法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容',
     },
     {
+      id: 3,
       rowNo: '第三条',
       // eslint-disable-next-line
       rowContent: '法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容',
     },
     {
+      id: 4,
       rowNo: '第四条',
       // eslint-disable-next-line
       rowContent: '法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容',
     },
   ]
 }, {
+  id: 'second',
   rowNo: '中华人民共和国海商法',
   rowContent: '',
   children: [
     {
+      id: 5,
       rowNo: '第一条',
       // eslint-disable-next-line
       rowContent: '法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容',
     },
     {
+      id: 6,
       rowNo: '第二条',
       // eslint-disable-next-line
       rowContent: '法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容',
     },
     {
+      id: 7,
       rowNo: '第三条',
       // eslint-disable-next-line
       rowContent: '法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容',
     },
     {
+      id: 8,
       rowNo: '第四条',
       // eslint-disable-next-line
       rowContent: '法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容',
     },
   ]
 }, {
+  id: 'third',
   rowNo: '纳税担保试行方法',
   rowContent: '',
   children: [
     {
+      id: 9,
       rowNo: '第一条',
       // eslint-disable-next-line
       rowContent: '法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容',
     },
     {
+      id: 10,
       rowNo: '第二条',
       // eslint-disable-next-line
       rowContent: '法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容',
     },
     {
+      id: 11,
       rowNo: '第三条',
       // eslint-disable-next-line
       rowContent: '法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容',
     },
     {
+      id: 12,
       rowNo: '第四条',
       // eslint-disable-next-line
       rowContent: '法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容',
     },
   ]
 }, {
+  id: 'fourth',
   rowNo: '中华人民共和国担保法',
   rowContent: '',
   children: [
     {
+      id: 13,
       rowNo: '第一条',
       // eslint-disable-next-line
       rowContent: '法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容',
     },
     {
+      id: 14,
       rowNo: '第二条',
       // eslint-disable-next-line
       rowContent: '法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容',
     },
     {
+      id: 15,
       rowNo: '第三条',
       // eslint-disable-next-line
       rowContent: '法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容',
     },
     {
+      id: 16,
       rowNo: '第四条',
       // eslint-disable-next-line
       rowContent: '法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容',
     },
   ]
 }, {
+  id: 'fifth',
   rowNo: '纳税担保试行方法',
   rowContent: '',
   children: [
     {
+      id: 17,
       rowNo: '第一条',
       // eslint-disable-next-line
       rowContent: '法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容',
     },
     {
+      id: 18,
       rowNo: '第二条',
       // eslint-disable-next-line
       rowContent: '法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容',
     },
     {
+      id: 19,
       rowNo: '第三条',
       // eslint-disable-next-line
       rowContent: '法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容',
     },
     {
+      id: 20,
       rowNo: '第四条',
       // eslint-disable-next-line
       rowContent: '法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容法律条文内容',
@@ -160,36 +185,12 @@ export default function SingleTreeTable() {
     <div className={styles.container}>
       <Table
         dataSource={data}
-        primaryKey="rowNo"
+        primaryKey="id"
         isTree
-        cellProps={(rowIndex, colIndex) => {
-          if (rowIndex === 0 && colIndex === 0) {
+        cellProps={(rowIndex, colIndex, dataIndex, record) => {
+          if (record.children && colIndex === 0) {
             return {
-              colSpan: 2
-            };
-          }
-  
-          if (rowIndex === 5 && colIndex === 0) {
-            return {
-              colSpan: 2
-            };
-          }
-
-          if (rowIndex === 10 && colIndex === 0) {
-            return {
-              colSpan: 2
-            };
-          }
-
-          if (rowIndex === 15 && colIndex === 0) {
-            return {
-              colSpan: 2
-            };
-          }
-
-          if (rowIndex === 20 && colIndex === 0) {
-            return {
-              colSpan: 2
+              colSpan: 2,
             };
           }
         }}
