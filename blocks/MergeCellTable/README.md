9e696b10179f3dbd5c6b48705a82f58adc5aa661
暮尘 <i@muchen.me>
2020-06-23 16:38:18 +0800

    feat: 完成合并表格

--- blocks/MergeCellTable/README.md
+++ blocks/MergeCellTable/README.md
@@ -1,5 +1,53 @@
 # MergeCellTable
 
-简介：合并单元格Table
+简介：合并单元格 Table
 
 合并单元格的 Table
+
+原始数据常见的两种
+
+```javascript
+const dataSource = [
+  { id: "1", name: "xx", group: "a" },
+  { id: "2", name: "xx", group: "a" },
+  { id: "3", name: "xx", group: "a" },
+  { id: "4", name: "xx", group: "b" },
+  { id: "5", name: "xx", group: "b" },
+  { id: "6", name: "xx", group: "b" },
+];
+```
+
+```javascript
+const dataSource = [
+  {
+    group: "a",
+    children: [
+      { id: "1", name: "xx" },
+      { id: "2", name: "xx" },
+      { id: "3", name: "xx" },
+    ],
+  },
+  {
+    group: "b",
+    children: [
+      { id: "1", name: "xx" },
+      { id: "2", name: "xx" },
+      { id: "3", name: "xx" },
+    ],
+  },
+];
+```
+
+为了实现合并单元格, 需要对数据源做转换, 手动统计出 groupCount 数量以及 分组内的排序, 并且保证同一分组排在相邻的位置。
+上面两例 dataSource 是等价的, 转换后的结构是一致的
+
+```json
+[
+  { "id": "1", "name": "xx", "group": "a", "groupCount": 3, "groupIndex": 0 },
+  { "id": "2", "name": "xx", "group": "a", "groupCount": 3, "groupIndex": 1 },
+  { "id": "3", "name": "xx", "group": "a", "groupCount": 3, "groupIndex": 2 },
+  { "id": "4", "name": "xx", "group": "b", "groupCount": 3, "groupIndex": 0 },
+  { "id": "5", "name": "xx", "group": "b", "groupCount": 3, "groupIndex": 1 },
+  { "id": "6", "name": "xx", "group": "b", "groupCount": 3, "groupIndex": 2 }
+]
+```
--- blocks/MergeCellTable/src/index.tsx
+++ blocks/MergeCellTable/src/index.tsx
@@ -1,20 +1,97 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
+import { Table, Pagination } from '@alifd/next';
+import { useFusionTable } from 'ahooks';
 
-import styles from './index.module.scss';
+const MockData = [
+  {
+    gender: 'female',
+    name: 'Sandø',
+    email: 'melissa.sando@example.com',
+    phone: '82757809',
+    id: '27096632423',
+    groupCount: 2,
+    groupIndex: 0,
+  },
+  {
+    gender: 'female',
+    name: 'Sandø',
+    email: 'torvald.ihle@example.com',
+    phone: '(18) 2827578509',
+    id: '27096632423',
+    groupCount: 2,
+    groupIndex: 1,
+  },
+  {
+    gender: 'male',
+    name: 'Ortega',
+    email: 'jesus.ortega@example.com',
+    phone: '902-264-345',
+    id: '67967111-Y',
+    groupCount: 3,
+    groupIndex: 0,
+  },
+  {
+    gender: 'male',
+    name: 'Ortega',
+    email: 'dositeu.daluz@example.com',
+    phone: '(98) 2534-5239',
+    id: '0b2dffc6a3e282dbee19b432371a1248',
+    groupCount: 3,
+    groupIndex: 1,
+  },
+  {
+    gender: 'male',
+    name: 'Ortega',
+    email: 'marcel.rousseau@example.com',
+    phone: '077 383 91 73',
+    id: '3c51c6551a2a263ee1626d84e6426ea53fa60a3a',
+    groupCount: 3,
+    groupIndex: 2,
+  },
+  {
+    gender: 'male',
+    name: 'Berger',
+    email: 'evan.berger@example.com',
+    phone: '02-46-03-53-36',
+    id: '1NNaN50556983-14',
+    groupCount: 1,
+    groupIndex: 0,
+  },
+];
 
-export default function MergeCellTable({ value }) {
+const getTableData = () =>
+  Promise.resolve({
+    total: 55,
+    list: MockData,
+  });
+
+const cellProps = (rowIndex, colIndex, dataIndex, record) => {
+  // 只合并 name & gender
+  if (colIndex === 0 || colIndex === 1) {
+    const { groupCount, groupIndex } = record;
+    if (groupIndex === 0) {
+      return { rowSpan: groupCount };
+    }
+  }
+};
+
+export default function MergeCellTable() {
+  const { paginationProps, tableProps, loading } = useFusionTable(
+    getTableData,
+    {}
+  );
   return (
-    <div className={styles.container}>
-      MergeCellTable {value}
-    </div>
+    <>
+      <Table {...tableProps} primaryKey="email" cellProps={cellProps}>
+        <Table.Column title="name" dataIndex="name" width={240} />
+        <Table.Column title="gender" dataIndex="gender" width={500} />
+        <Table.Column title="email" dataIndex="email" width={500} />
+        <Table.Column title="phone" dataIndex="phone" width={500} />
+      </Table>
+      <Pagination
+        style={{ marginTop: 16, textAlign: 'right' }}
+        {...paginationProps}
+      />
+    </>
   );
 }
-
-MergeCellTable.propTypes = {
-  value: PropTypes.string,
-};
-
-MergeCellTable.defaultProps = {
-  value: 'block',
-};
