c1a6c2159d68ab1e696b05580ca50eb06d4f9cbd
sml782 <sml782@163.com>
2020-07-06 17:27:29 +0800

    fix: getColumnKey return type error

--- blocks/ActionTable/src/util.tsx
+++ blocks/ActionTable/src/util.tsx
@@ -1,6 +1,6 @@
 import { ColumnProps } from '@alifd/next/types/table';
 
-export function getColumnKey(column: ColumnProps & { key?: string }): string {
+export function getColumnKey(column: ColumnProps & { key?: string }): string | null {
   if (column) {
     return column.key || String(column.title) || column.dataIndex;
   }
