b3cbf049f10bc58a86b5b81c5701686c00390f86
sml782 <sml782@163.com>
2020-07-08 10:18:56 +0800

    feat: complete fusion-dialog-table

---
+++ blocks/DialogTable/src/DialogOperation.tsx
@@ -0,0 +1,50 @@
+import React, { useRef } from 'react';
+import { Dialog } from '@alifd/next';
+import { DialogProps } from '@alifd/next/types/dialog';
+
+import Operation, { ActionType, OperaitionProps, OperationRef } from './Operation';
+
+const getDialogProps = (actionType: ActionType): string => {
+  switch (actionType) {
+    case 'add':
+    default:
+      return '添加员工';
+
+    case 'edit':
+      return '编辑员工';
+
+    case 'preview':
+      return '预览员工';
+  }
+};
+
+const DialogOperation: React.FC<OperaitionProps & DialogProps> = (props) => {
+  const { actionType, dataSource, onOk, ...lastProps } = props;
+  const optionRef = useRef<OperationRef>(null);
+  
+  const handleOk = (event: Event) => {
+    if (actionType === 'preview') {
+      return onOk(event);
+    }
+    optionRef.current.getValues((values) => {
+      console.log(values);
+      onOk(event);
+    });
+  }
+
+  return (
+    <Dialog
+      shouldUpdatePosition
+      isFullScreen
+      title={getDialogProps(actionType)}
+      style={{ width: 600 }}
+      footerAlign="center"
+      {...lastProps}
+      onOk={handleOk}
+    >
+      <Operation ref={optionRef} actionType={actionType} dataSource={dataSource} />
+    </Dialog>
+  );
+}
+
+export default DialogOperation;
