64938a938ee9100cf733571a02e820f04569a113
alvinhui <alvin.hui@qq.com>
2020-09-04 11:52:35 +0800

    style: fix by lint

--- pages/ListPage/src/response.ts
+++ pages/ListPage/src/response.ts
@@ -1,118 +1,118 @@
 export default {
   data: {
-    "data": [
+    'data': [
       {
-        "id": 162,
-        "gmtCreate": 1598930400000,
-        "gmtModified": 1599034288000,
-        "name": "简洁版升级测试",
-        "bizCode": "tmall_fs",
-        "seasonCode": 4,
-        "seasonName": "2020年夏",
-        "propertyList": [
+        'id': 162,
+        'gmtCreate': 1598930400000,
+        'gmtModified': 1599034288000,
+        'name': '简洁版升级测试',
+        'bizCode': 'tmall_fs',
+        'seasonCode': 4,
+        'seasonName': '2020年夏',
+        'propertyList': [
           {
-            "id": 1,
-            "propertyName": "图案",
-            "necessary": true
+            'id': 1,
+            'propertyName': '图案',
+            'necessary': true
           },
           {
-            "id": 39,
-            "propertyName": "颜色",
-            "necessary": true
+            'id': 39,
+            'propertyName': '颜色',
+            'necessary': true
           },
           {
-            "id": 60,
-            "propertyName": "面料",
-            "necessary": true
+            'id': 60,
+            'propertyName': '面料',
+            'necessary': true
           }
         ],
-        "logo": "https://img.alicdn.com/imgextra/i2/6000000007713/O1CN0126qbTRpwI6z6cmE_!!6000000007713-0-tmtrend.jpg",
-        "status": "Published",
-        "url": "https://pre-wormhole.wapa.tmall.com/wow/a/act/tmall/dailygroup/1920/wupr?wh_pid=daily-206309&key=qn_1aaf99c9d6a04df6bcf46c557c8de795",
-        "type": "Yangyi",
-        "questionnaireId": 175,
-        "sellerId": 263681408,
-        "progress": 0,
-        "itemSize": 6,
-        "statusDesc": "投放中",
-        "publishTime": 1599034288000
+        'logo': 'https://img.alicdn.com/imgextra/i2/6000000007713/O1CN0126qbTRpwI6z6cmE_!!6000000007713-0-tmtrend.jpg',
+        'status': 'Published',
+        'url': 'https://pre-wormhole.wapa.tmall.com/wow/a/act/tmall/dailygroup/1920/wupr?wh_pid=daily-206309&key=qn_1aaf99c9d6a04df6bcf46c557c8de795',
+        'type': 'Yangyi',
+        'questionnaireId': 175,
+        'sellerId': 263681408,
+        'progress': 0,
+        'itemSize': 6,
+        'statusDesc': '投放中',
+        'publishTime': 1599034288000
       },
       {
-        "id": 120,
-        "gmtCreate": 1597393024000,
-        "gmtModified": 1597393030000,
-        "name": "回归2",
-        "bizCode": "tmall_fs",
-        "seasonCode": 4,
-        "seasonName": "2020年夏",
-        "propertyList": [
+        'id': 120,
+        'gmtCreate': 1597393024000,
+        'gmtModified': 1597393030000,
+        'name': '回归2',
+        'bizCode': 'tmall_fs',
+        'seasonCode': 4,
+        'seasonName': '2020年夏',
+        'propertyList': [
           {
-            "id": 1,
-            "propertyName": "图案",
-            "necessary": true
+            'id': 1,
+            'propertyName': '图案',
+            'necessary': true
           },
           {
-            "id": 39,
-            "propertyName": "颜色",
-            "necessary": true
+            'id': 39,
+            'propertyName': '颜色',
+            'necessary': true
           },
           {
-            "id": 60,
-            "propertyName": "面料",
-            "necessary": true
+            'id': 60,
+            'propertyName': '面料',
+            'necessary': true
           }
         ],
-        "logo": "https://img.alicdn.com/imgextra/i4/6000000008105/O1CN011E5kzh29k8j91hUCg_!!6000000008105-0-tmtrend.jpg",
-        "status": "Published",
-        "url": "https://pages.tmall.com/wow/a/act/tmall/dailygroup/1925/wupr?wh_pid=daily-206364&key=qn_33a476548f5443afa7a2fec54e4eb3fc",
-        "type": "Yangyi",
-        "questionnaireId": 133,
-        "sellerId": 263681408,
-        "progress": 0,
-        "itemSize": 6,
-        "statusDesc": "投放中",
-        "publishTime": 1597393030000
+        'logo': 'https://img.alicdn.com/imgextra/i4/6000000008105/O1CN011E5kzh29k8j91hUCg_!!6000000008105-0-tmtrend.jpg',
+        'status': 'Published',
+        'url': 'https://pages.tmall.com/wow/a/act/tmall/dailygroup/1925/wupr?wh_pid=daily-206364&key=qn_33a476548f5443afa7a2fec54e4eb3fc',
+        'type': 'Yangyi',
+        'questionnaireId': 133,
+        'sellerId': 263681408,
+        'progress': 0,
+        'itemSize': 6,
+        'statusDesc': '投放中',
+        'publishTime': 1597393030000
       },
       {
-        "id": 119,
-        "gmtCreate": 1597392201000,
-        "gmtModified": 1597392234000,
-        "name": "回归测试",
-        "bizCode": "tmall_fs",
-        "seasonCode": 3,
-        "seasonName": "2020年春",
-        "propertyList": [
+        'id': 119,
+        'gmtCreate': 1597392201000,
+        'gmtModified': 1597392234000,
+        'name': '回归测试',
+        'bizCode': 'tmall_fs',
+        'seasonCode': 3,
+        'seasonName': '2020年春',
+        'propertyList': [
           {
-            "id": 1,
-            "propertyName": "图案",
-            "necessary": true
+            'id': 1,
+            'propertyName': '图案',
+            'necessary': true
           },
           {
-            "id": 39,
-            "propertyName": "颜色",
-            "necessary": true
+            'id': 39,
+            'propertyName': '颜色',
+            'necessary': true
           },
           {
-            "id": 60,
-            "propertyName": "面料",
-            "necessary": true
+            'id': 60,
+            'propertyName': '面料',
+            'necessary': true
           }
         ],
-        "logo": "https://img.alicdn.com/imgextra/i3/6000000006085/O1CN01lY5Z3T1uoyfrmYTdZ_!!6000000006085-0-tmtrend.jpg",
-        "status": "Published",
-        "url": "https://pages.tmall.com/wow/a/act/tmall/dailygroup/1920/wupr?wh_pid=daily-206308&key=qn_81e4fc606a10417388fd9205299d2ede",
-        "type": "Yangyi",
-        "questionnaireId": 132,
-        "sellerId": 263681408,
-        "progress": 0,
-        "itemSize": 6,
-        "statusDesc": "投放中",
-        "publishTime": 1597392234000
+        'logo': 'https://img.alicdn.com/imgextra/i3/6000000006085/O1CN01lY5Z3T1uoyfrmYTdZ_!!6000000006085-0-tmtrend.jpg',
+        'status': 'Published',
+        'url': 'https://pages.tmall.com/wow/a/act/tmall/dailygroup/1920/wupr?wh_pid=daily-206308&key=qn_81e4fc606a10417388fd9205299d2ede',
+        'type': 'Yangyi',
+        'questionnaireId': 132,
+        'sellerId': 263681408,
+        'progress': 0,
+        'itemSize': 6,
+        'statusDesc': '投放中',
+        'publishTime': 1597392234000
       }
     ],
-    "totalCount": 74,
-    "currentPage": 1,
-    "pageSize": 10
+    'totalCount': 74,
+    'currentPage': 1,
+    'pageSize': 10
   },
   success: true,
 }
