ab6d1755b8879ec59ed3ed9bdc7a874b2f87747e
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2020-11-25 12:02:13 +0800

    chore: optimize typings

--- scaffolds/fusion-design-pro/src/typings.d.ts
+++ scaffolds/fusion-design-pro/src/typings.d.ts
@@ -2,3 +2,13 @@ declare module '*.module.scss' {
   const classes: { [key: string]: string };
   export default classes;
 }
+
+declare module '*.module.less' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module '*.module.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
