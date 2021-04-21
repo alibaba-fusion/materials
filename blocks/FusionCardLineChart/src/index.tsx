d16df5dd777d5d0a40c32b0a011c591ef34445b6
wb-tzx726756 <wb-tzx726756@alibaba-inc.com>
2021-04-21 16:34:54 +0800

    fix: block FusionCardLineChart

--- blocks/FusionCardLineChart/src/index.tsx
+++ blocks/FusionCardLineChart/src/index.tsx
@@ -3,7 +3,7 @@ import { Card } from '@alifd/next';
 import { Chart, Geom } from 'bizcharts';
 import mock from './mock.js';
 
-import styles from './index.module.scss';
+import styles from './index.module.css';
 
 interface ChartItem {
   date?: string;
@@ -51,9 +51,9 @@ const FusionCardLineChart: React.FunctionComponent<FusionCardLineChartProps> = (
         ) : null
       }
       <Card.Content>
-        <div className={styles.subTitle}>{subTitle}</div>
-        <div className={styles.value}>{value}</div>
-        <div className={styles.des}>{des}<span>{rate}↑</span></div>
+        <div className={styles.CardSubTitle}>{subTitle}</div>
+        <div className={styles.CardValue}>{value}</div>
+        <div className={styles.CardDes}>{des}<span>{rate}↑</span></div>
         <Chart
           width={10}
           height={chartHeight}
