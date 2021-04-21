d398afa50e2985bb3102f72974e352225ab9a055
wb-tzx726756 <wb-tzx726756@alibaba-inc.com>
2021-04-21 15:06:07 +0800

    fix: block FusionCardGroupBarChart

--- scripts/tsChecker.js
+++ scripts/tsChecker.js
@@ -47,7 +47,7 @@ function dtsCompiler(targetFolder) {
 
 // const blocks = fse.readdirSync(path.join(__dirname, '../blocks'));
 // 修改一个区块添加一个检测
-const blocks = ['ActionTable','AdvancedDetail','BasicDetail','BasicForm','BasicList','CardList'];
+const blocks = ['ActionTable','AdvancedDetail','BasicDetail','BasicForm','BasicList','CardList','FusionCardAreaChart','FusionCardCrossPieChart','FusionCardBarChart','FusionCardGroupBarChart'];
 
 for (const block of blocks) {
   const blockFolder = path.join(__dirname, `../blocks/${block}`);
