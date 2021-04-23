38531fb70107f810f56cf34babac0e6007f1f3c0
wb-tzx726756 <wb-tzx726756@alibaba-inc.com>
2021-04-23 14:59:26 +0800

    fix: block GroupForm

--- scripts/tsChecker.js
+++ scripts/tsChecker.js
@@ -47,7 +47,7 @@ function dtsCompiler(targetFolder) {
 
 // const blocks = fse.readdirSync(path.join(__dirname, '../blocks'));
 // 修改一个区块添加一个检测
-const blocks = ['ActionTable','AdvancedDetail','BasicDetail','BasicForm','BasicList','CardList'];
+const blocks = ['ActionTable', 'AdvancedDetail', 'BasicDetail', 'BasicForm', 'BasicList', 'CardList', 'FusionCardTypebarChart', 'FusionCardWorldmapChart', 'GroupForm'];
 
 for (const block of blocks) {
   const blockFolder = path.join(__dirname, `../blocks/${block}`);
@@ -63,5 +63,5 @@ for (const block of blocks) {
   } catch (err) {
     console.log(err.message);
     fse.removeSync(dtsFile)
-  } 
+  }
 }
