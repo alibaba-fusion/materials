2ce7141b39bff58370e3862e35848bff24b220b6
wb-tzx726756 <wb-tzx726756@alibaba-inc.com>
2021-04-23 15:43:07 +0800

    fix: block LoginBlock

--- blocks/LoginBlock/src/index.tsx
+++ blocks/LoginBlock/src/index.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Input, Message, Form, Divider, Checkbox, Icon } from '@alifd/next';
 
 import { useInterval } from './utils';
-import styles from './index.module.scss';
+import styles from './index.module.css';
 
 const { Item } = Form;
 
