5d89c1ada89fe312bf0de4c0359bb7b8ff50fb33
wb-tzx726756 <wb-tzx726756@alibaba-inc.com>
2021-04-26 17:24:58 +0800

    fix: block ActionTable

--- blocks/ActionTable/src/index.tsx
+++ blocks/ActionTable/src/index.tsx
@@ -8,7 +8,7 @@ import { getColumnKey } from './util';
 
 import styles from './index.module.css';
 
-//@ts-ignore
+// @ts-ignore
 const TableActionIcon = Icon.createFromIconfontCN({
   scriptUrl: '//at.alicdn.com/t/font_1899388_oxn3zhg34oj.js',
 });
