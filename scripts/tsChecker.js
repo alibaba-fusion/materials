52d56ca3f23431eae77a7801a0cf2566378b071a
tzx-Wine <82510763+tzx-Wine@users.noreply.github.com>
2021-04-29 15:13:12 +0800

    fix: block FlowForm Forbidden FourColumnForm (#111)

--- scripts/tsChecker.js
+++ scripts/tsChecker.js
@@ -46,7 +46,7 @@ function dtsCompiler(targetFolder) {
 
 // const blocks = fse.readdirSync(path.join(__dirname, '../blocks'));
 // 修改一个区块添加一个检测
-const blocks = ['ActionTable','AdvancedDetail','BasicDetail','BasicForm','BasicList','CardList'];
+const blocks = ['ActionTable', 'AdvancedDetail', 'BasicDetail', 'BasicForm', 'BasicList', 'CardList', 'FlowForm', 'Forbidden', 'FourColumnForm'];
 
 for (const block of blocks) {
   const blockFolder = path.join(__dirname, `../blocks/${block}`);
@@ -61,5 +61,5 @@ for (const block of blocks) {
   } catch (err) {
     fse.removeSync(dtsFile)
     throw err;
-  } 
+  }
 }
