baa72bbacd67607a9af8b7ecd01df76e2a763efa
ClarkXia <xiawenwu41@gmail.com>
2021-05-12 20:28:53 +0800

    fix: ts checker scripts

--- scripts/tsChecker.js
+++ scripts/tsChecker.js
@@ -18,7 +18,7 @@ const options = {
 function dtsCompiler(targetFolder) {
   const needCompileList = globby.sync(['**/*.ts?(x)'], {
     cwd: targetFolder,
-    ignore: ['node_modules', '*.d.ts'],
+    ignore: ['node_modules'],
     absolute: true,
   });
   if (needCompileList.length === 0) {
@@ -29,7 +29,7 @@ function dtsCompiler(targetFolder) {
   const host = ts.createCompilerHost(options);
 
   // Prepare and emit the d.ts files
-  const program = ts.createProgram(needCompileList, options, host);
+  const program = ts.createProgram([...needCompileList, path.join(__dirname, '../types/typings.d.ts')], options, host);
   const emitResult = program.emit();
   if (emitResult.diagnostics && emitResult.diagnostics.length > 0) {
     emitResult.diagnostics.forEach(diagnostic => {
@@ -43,22 +43,25 @@ function dtsCompiler(targetFolder) {
       throw new Error('ts checker failed');
     });
   }
+  try {
+    needCompileList.forEach((filePath) => {
+      fse.unlinkSync(filePath.replace(/\.ts(x)?$/, '.d.ts'));
+    });
+  } catch(err) {
+    console.log(err);
+  }
 };
 
 const blocks = fse.readdirSync(path.join(__dirname, '../blocks'));
 
 for (const block of blocks) {
   const blockFolder = path.join(__dirname, `../blocks/${block}`);
-  const dtsFile = path.join(blockFolder, 'src', 'typings.d.ts');
   try {
     execSync(`cd blocks/${block} && npm i`, {
       stdio: 'inherit'
     });
-    fse.copyFileSync(path.join(__dirname, '../types/typings.d.ts'), dtsFile);
     dtsCompiler(blockFolder);
-    fse.removeSync(dtsFile)
   } catch (err) {
-    fse.removeSync(dtsFile)
     throw err;
   }
 }
