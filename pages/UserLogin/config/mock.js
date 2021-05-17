f1d04af3a6f75b84c6cccce2b06d9a9f578f1a8d
大果 <sobear.me@gmail.com>
2021-05-17 11:08:00 +0800

    feat: add IRouterConfig and try webpack5 (#116)
    * chore: add CHANGELOG
    
    * chore: fix eslint
    
    * feat: add IRouterConfig ts type (#101)
    
    * feat: add IRouterConfig ts type
    
    * fix: eslint error
    
    * feat: add plugin-webpack5 for pro (#99)
    
    * feat: add plugin-webpack5
    
    * chore: version
    
    * fix: lint
    
    Co-authored-by: liuxiong.lx <liuxiong.lx@alibaba-inc.com>
    
    * chore: modify changelog
    
    * chore: remove plugin-fusion-material
    
    * chore: regenerate
    
    * chore: publish scaffolds
    
    * chore: remove pages
    
    Co-authored-by: Hengchang Lu <44047106+luhc228@users.noreply.github.com>
    Co-authored-by: ClarkXia <xiawenwu41@gmail.com>

--- pages/UserLogin/config/mock.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  isShowOther: true,
-  isShowRegister: true,
-  isShowAutoLogin: false,
-  loginBtnStr: '登录',
-};
--- scaffolds/ice-stark-layout/src/routes.ts
+++ scaffolds/ice-stark-layout/src/routes.ts
@@ -1,9 +1,10 @@
+import { IRouterConfig } from 'ice';
 import Home from '@/pages/Home';
 import About from '@/pages/About';
 import Login from '@/pages/Login';
 import NotFound from '@/components/NotFound';
 
-const routes = [{
+const routes: IRouterConfig[] = [{
   path: '/about',
   component: About,
 }, {
